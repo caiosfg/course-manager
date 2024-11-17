f0cb1215a1906fc79894325975280bc35f7163a5
caiosfg <caio.sfg@gmail.com>
2024-11-17 00:59:50 -0300

    versioning api

--- api/app/Http/Controllers/Api/V1/CourseController.php
+++ api/app/Http/Controllers/Api/V1/CourseController.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\V1;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreCourseRequest;
 use App\Http\Requests\UpdateCourseRequest;
 use App\Models\Course;
+
 
 class CourseController extends Controller
 {
@@ -13,7 +15,7 @@ class CourseController extends Controller
      */
     public function index()
     {
-        //
+        return Course::all();
     }
 
     /**
